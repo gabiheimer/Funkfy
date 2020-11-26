56122618c9c30c3a602f15a68dcc61017dd08525
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-11-25 21:45:34 -0300

    Create POST routes for songs

--- song-api/app.py
+++ song-api/app.py
@@ -3,17 +3,33 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/song', methods=['GET'])
-def get_song():
-  data = open('/app/api/audio/watermelon-sugar.mp3', 'rb')
+@app.route('/songs/<title>', methods=['GET'])
+def get_song(title):
+  data = open('/song-api/' + title + '/song.mp3', 'rb')
   return Response(data, mimetype='audio/mp3')
 
-@app.route('/song', methods=['POST'])
-def receive_song():
-  body = request.data
-  newFile = open("request.mp3", "wb")
-  newFileByteArray = body
-  # logs = open("logs.data", "w")
-  # logs.write(newFileByteArray)
-  newFile.write(newFileByteArray)
-  return ('', 200)
+@app.route('/songs/<title>', methods=['POST'])
+def receive_song(title):
+  song = request.data
+  return save_song(title, 'song')
+
+@app.route('/songs/<title>/vocals', methods=['POST'])
+def receive_song_vocals(title):
+  song = request.data
+  return save_song(title, 'vocals')
+
+@app.route('/songs/<title>/accompaniment', methods=['POST'])
+def receive_song_accompaniment(title):
+  song = request.data
+  return save_song(title, 'accompaniment')
+
+@app.route('/songs/<title>/result', methods=['POST'])
+def receive_song_result(title):
+  song = request.data
+  return save_song(title, 'result')
+
+def save_song(title, part):
+  song = request.data
+  newFile = open("/song-api/" + title + "/" + part + ".mp3", "wb")
+  newFile.write(song)
+  return('', 201)
