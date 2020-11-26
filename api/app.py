0f1eb455e23e1252d22a86a0d7e3f0562985d110
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-11-26 04:59:13 -0300

    Remove unused routes

--- api/app.py
+++ api/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, Response
 from flask_cors import CORS
-from pydub import AudioSegment, effects
 import pika
 import json
 import requests
@@ -70,38 +69,6 @@ def get_result():
     r = requests.get(url, body=request.body, headers=request.headers)
     return r.content
 
-
-@app.route("/song/merge", methods=['PATCH'])
-def audio():
-    body = request.json
-    vocals = body['vocals']
-    accompaniment = body['accompaniment']
-    vocals_file = body['vocals_file']
-    accompaniment_file = body['accompaniment_file']
-    sound1 = AudioSegment.from_file('/app/api/output/' + vocals + vocals_file)
-    sound2 = AudioSegment.from_file(
-        '/app/api/output/' + accompaniment + accompaniment_file) - 20
-
-    combined = sound1.overlay(sound2)
-
-    combined.export('/app/api/merged/' + vocals + '-' +
-                    accompaniment + '.mp3', format='mp3')
-    return {
-        'status': 'Created :D'
-    }
-
-
-@app.route('/song/split', methods=['PATCH'])
-def split_song():
-    body = request.json
-    song_title = body['title']
-    stems = body['stems']
-
-    os.system('spleeter separate -i /app/api/audio/' + song_title +
-              ' -p spleeter:' + str(stems) + 'stems -o /app/api/output')
-    return {}
-
-
 @app.route('/song', methods=['GET'])
 def get_song():
     data = open('/app/api/audio/watermelon-sugar.mp3', 'rb')
@@ -113,10 +80,5 @@ def receive_song():
     body = request.data
     newFile = open("request.mp3", "wb")
     newFileByteArray = body
-    # logs = open("logs.data", "w")
-    # logs.write(newFileByteArray)
     newFile.write(newFileByteArray)
     return ('', 200)
-
-
-app.run()
